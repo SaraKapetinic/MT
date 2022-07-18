dfd708ea7912f05d199a57ddebaecfcf2f768829
Sara Kapetinic <sara.kapetinic.sk@gmail.com>
2022-07-18 16:40:58 +0200

    Project started

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module main.mojtermin {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens main.mojtermin to javafx.fxml;
+    exports main.mojtermin;
+}
